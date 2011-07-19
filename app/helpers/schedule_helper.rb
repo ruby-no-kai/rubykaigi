1a68cf8d9fa6a88a12a5851e6088b4b111c345bc
Eito Katagiri <eitoball@gmail.com>
2011-07-20 00:44:39 +0900

    [#15944931] rearrange vimeo url

--- app/helpers/schedule_helper.rb
+++ app/helpers/schedule_helper.rb
@@ -1,7 +1,15 @@
 # coding: utf-8
 
 module ScheduleHelper
-  def vimeo_url(event)
-    event.vimeo_id ? "http://vimeo.com/#{event.vimeo_id}" : ''
+  def vimeo_frame(vimeo_id)
+    return '' if vimeo_id.blank?
+    capture_haml do
+      haml_tag :iframe, :frameborder => '0', :width => '400', :height => '300', :src => "http://player.vimeo.com/video/#{vimeo_id}"
+      haml_tag :div do
+        haml_tag :a, :href => "http://vimeo.com/#{vimeo_id}", :target => '_blank' do
+          haml_concat "http://vimeo.com/#{vimeo_id}"
+        end
+      end
+    end
   end
 end
--- spec/helpers/schedule_helper_spec.rb
+++ spec/helpers/schedule_helper_spec.rb
@@ -2,14 +2,32 @@
 require 'spec_helper'
 
 describe ScheduleHelper do
-  describe '.vimeo_url' do
-    context 'event with vimeo_id' do
-      before { stub(@event = RubyKaigi2011::Event.new).vimeo_id { '123' } }
-      it { helper.vimeo_url(@event).should == 'http://vimeo.com/123' }
+  before do
+    helper.extend Haml
+    helper.extend Haml::Helpers
+    helper.send :init_haml_helpers
+  end
+
+  context '.video_frame' do
+    describe '(nil)' do
+      it { helper.vimeo_frame(nil).should be_blank }
     end
-    context 'event without vimeo_id' do
-      before { stub(@event = RubyKaigi2011::Event.new).vimeo_id { nil } }
-      it { helper.vimeo_url(@event).should be_blank }
+
+    describe '(123)' do
+      let(:res) { helper.vimeo_frame([123]) }
+      it 'should have embeded vimeo player frame' do
+        doc = Nokogiri.HTML(res)
+        iframes = doc.xpath('//iframe')
+        iframes.should have(1).iframe
+        iframes.first['src'].should match(%r{http://player.vimeo.com/video/[0-9]+})
+      end
+
+      it 'should have link to vimeo page' do
+        doc = Nokogiri.HTML(res)
+        links = doc.xpath('//a')
+        links.should have(1).link
+        links.first['href'].should match(%r{http://vimeo.com/[0-9]+})
+      end
     end
   end
 end
