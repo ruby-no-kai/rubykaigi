b5cf123746a15363347b1c8d53ceedfb4535f407
Eito Katagiri <eitoball@gmail.com>
2011-07-22 22:45:25 +0900

    refactor a spec

--- spec/helpers/schedule_helper_spec.rb
+++ spec/helpers/schedule_helper_spec.rb
@@ -8,25 +8,23 @@ describe ScheduleHelper do
     helper.send :init_haml_helpers
   end
 
-  context '.video_frame' do
+  describe '.video_frame' do
     describe '(nil)' do
       it { helper.vimeo_frame(nil).should be_blank }
     end
 
     describe '(123)' do
       let(:res) { helper.vimeo_frame([123]) }
+      let(:doc) { Nokogiri.HTML(res) }
+
+      let(:iframe) { doc.xpath('//iframe').first }
       it 'should have embeded vimeo player frame' do
-        doc = Nokogiri.HTML(res)
-        iframes = doc.xpath('//iframe')
-        iframes.should have(1).iframe
-        iframes.first['src'].should match(%r{http://player.vimeo.com/video/[0-9]+})
+        iframe['src'].should match(%r{http://player.vimeo.com/video/[0-9]+})
       end
 
+      let(:link) { doc.xpath('//a').first }
       it 'should have link to vimeo page' do
-        doc = Nokogiri.HTML(res)
-        links = doc.xpath('//a')
-        links.should have(1).link
-        links.first['href'].should match(%r{http://vimeo.com/[0-9]+})
+        link['href'].should match(%r{http://vimeo.com/[0-9]+})
       end
     end
   end
